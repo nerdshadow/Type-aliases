7b50ca46611fda4b8f679d694b859c5a030d221c
Evilnood <evilnood666@gmail.com>
2021-03-16 16:03:55 +0200

    Добавьте файлы проекта.

---
+++ Type aliases.cpp
@@ -0,0 +1,18 @@
+﻿
+#include <iostream>
+
+typedef int Numbers;
+using AlsoNumbers = int;
+
+
+int main()
+{
+    int a = 341;
+    Numbers b = 5123;
+    AlsoNumbers c = 11328;
+
+    std::cout << a << " <- this is int"<<std::endl;
+    std::cout << b << " <- this also int"<<std::endl;
+    std::cout << c << " <- also int"<<std::endl;
+    std::cout << sizeof(a) <<" = "<< sizeof(b) << " = " << sizeof(c) << std::endl; // the same size 
+}
